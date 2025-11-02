98a49e610acd6590e250fb57b7957c76f78005e8
ztachi <legendryztachi@gmail.com>
2025-11-03 00:32:52 +0800

    改了点样式

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -1,4 +1,4 @@
-import { Card, Button } from 'antd';
+import { Card, Button, Col } from 'antd';
 
 import FloatButtonList from '@/components/floatButtonList';
 
@@ -18,14 +18,20 @@ const gameList = [
 const Games = () => (
   <div className="h-screen flex justify-center items-center">
     {gameList.map((game) => (
-      <Card key={game.url}>
-        <Card.Meta title={game.title} description={game.description} />
-        <div className="text-center mt-10">
-          <Button type="primary" target="_blank" href={game.url}>
-            Play
-          </Button>
-        </div>
-      </Card>
+      <Col
+        span={6}
+        key={game.url}
+        className="mr-[20px] transition-transform  hover:scale-125 hover:relative hover:z-10"
+      >
+        <Card>
+          <Card.Meta title={game.title} description={game.description} />
+          <div className="text-center mt-10">
+            <Button type="primary" target="_blank" href={game.url}>
+              Play
+            </Button>
+          </div>
+        </Card>
+      </Col>
     ))}
     <FloatButtonList />
   </div>
